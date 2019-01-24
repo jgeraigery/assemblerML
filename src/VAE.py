e605d9e7d6515d59aa4b71420a852e745b331c2b
kentevan <kentaevans88@gmail.com>
2019-01-23 20:09:46 -0800

    real first commit

---
+++ src/VAE.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 23 19:04:39 2019
+
+@author: Kent Evans
+
+Designing
+
+
+"""
+
+import torch
+import torch.nn as nn
+import torchvision as tv
+import matplotlib.pyplot as plt
+import numpy as np
+
+def class VAE (nn.module):
+    def __init__(self):
+        super().__init__()
+        
