ab677b63cec9805dc615dcebfc2efb6e852449bf
kentevan <kentaevans88@gmail.com>
2019-02-09 17:39:21 -0800

    fixed error in the integrator term

--- src/dcmotor_ss.py
+++ src/dcmotor_ss.py
@@ -23,9 +23,9 @@ class motor:
         self.A = np.array([[0,1.0,0],[-(self.R*self.b-self.Km*self.Kb)/(self.L*self.J),-(self.L*self.b+self.R*self.J)/(self.L*self.J),0],self.C])
         self.B = np.array([[0],[self.Km/(self.L*self.J)],[0.0]])
         self.D = np.array([0.0])
-        self.Q = np.diag([10,0,0])
+        self.Q = np.diag([100,0,.001])
         self.Rm = np.array([.0001])
-        self.H = np.concatenate((np.concatenate((self.A,-self.B*np.array(1/self.Rm[0])*np.transpose(self.B)),axis = 1),np.concatenate((self.Q,-np.transpose(self.A)),axis = 1)),axis = 0)
+        self.H = np.concatenate((np.concatenate((self.A,-self.B*np.array(1/self.Rm[0])*np.transpose(self.B)),axis = 1),np.concatenate((self.Q,-self.A.T),axis = 1)),axis = 0)
         #[self.T,self.Z,numEigs] = la.schur(self.H, output = 'complex',sort = "lhp")
         #[self.W,self.Y,numEigs] = la.schur(self.H, output = 'real',sort = "lhp")
         #[self.T,self.Z,numEigs] = la.schur(self.H, sort = "rhp")
@@ -38,7 +38,10 @@ class motor:
     def genRefTraj(self,t,y):
         self.t = t
         self.dT = t[1]-t[0]
-        self.xd = y
+        self.xd = np.zeros_like(t)
+        for i in range(len(t)):
+            self.xd[i] = self.ud(t[i])
+        #self.xd = y
         self.ud = np.array(self.xd*(self.R*self.b-self.Km*self.Kb)/self.Km)
         #self.ud = np.zeros_like(self.xd)
         self.xd = np.array([self.xd,np.zeros(self.xd.shape),np.zeros(self.xd.shape)])
@@ -46,13 +49,24 @@ class motor:
     def simulate(self):
         self.stateRes = np.zeros([self.xd.shape[0],len(self.t)])
         for i in range(len(self.t)):
-            statedot = np.matmul(self.A,self.state) + np.matmul(self.B,self.getInput(i))
+            statedot = np.matmul(self.A,self.state) + np.matmul(self.B,self.getInput(i)) - np.array([[0],[0],[1]])*self.xd[0,i]
             self.state += statedot*self.dT
             self.stateRes[:,i,None] = self.state
         plt.plot(self.t,self.stateRes[0,:],self.t,self.xd[0,:])
         #plt.axis([0,10,-1,1])
         #plt.show()
-        
+    
+    def ud(self,t):
+        if (t < 1):
+            return 1
+        elif (t<2):
+            return 5
+        elif (t<3):
+            return 3
+        elif (t<4):
+            return 9
+        else:
+            return 0
         
     def getInput(self,ind):
         #print(np.matmul(-self.K,self.state-self.xd[:,1,None]) + self.ud[ind])
@@ -60,7 +74,7 @@ class motor:
         
 a = motor()
 t = np.arange(0,10,.01)
-#y = np.sin(t)
-y = np.ones_like(t)
+y = np.sin(t)
+#y = np.ones_like(t)
 a.genRefTraj(t,y)
 a.simulate()
