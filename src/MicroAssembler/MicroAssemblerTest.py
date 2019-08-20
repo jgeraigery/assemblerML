5b50689f8a27fb86ccbade5434c770b2c51d5687
AnandRamakrishnan09 <aramakrishnan@compute-0-27.cm.cluster>
2019-08-19 21:03:37 -0400

    Cleaning Up

--- src/MicroAssembler/MicroAssemblerTest.py
+++ src/MicroAssembler/MicroAssemblerTest.py
@@ -19,6 +19,14 @@ import pandas as pd
 from keras.models import load_model
 from optparse import OptionParser
 
+from matplotlib.animation import FuncAnimation,FFMpegFileWriter
+#matplotlib.rcParams['animation.ffmpeg_args'] = '-report'
+matplotlib.rcParams['animation.bitrate'] = 2000
+
+
+
+
+
 #Writing Parser for Python File
 parser = OptionParser()
 
@@ -26,6 +34,7 @@ parser.add_option("-p", "--path", dest="test_path", help="Path to test data in t
 parser.add_option("-l", "--look_back", type="int", dest="look_back",help="Number of previous time_steps used when  training model ", default=1)
 parser.add_option("-f", "--predict_forward", type="int", dest="predict_forward",help="Number of future predicted time_steps when training model ", default=1)
 parser.add_option("--network", dest="network", help="Path to  Trained H5 File", default=None)
+parser.add_option("--video", dest="video",help="Saves Simulation as Video", default=None)
 
 (options, args) = parser.parse_args()
 
@@ -33,7 +42,7 @@ if not options.test_path:   # if filename is not given
         parser.error('Error: path to test data must be specified. Pass --path to command line')
 
 if not options.network:   	# if Network is
-        parser.error('Error: Please pass test Network')
+        parser.error('Error: Please pass test Network Weights in the format of a H5 file')
 
 #Taking Input annd Output Time_Step
 time_step=options.look_back
@@ -122,5 +131,68 @@ for i in range(len(X)):
 
 result=np.concatenate((result,time),axis=1)
 evaluate(result,output_size=output_size,Training_Time=0,name="Outputs/Testing-"+Name)
+
+
+
+if options.video is not None:
+	print ("Inside Video")
+	outresults=[]
+	out=np.zeros((1,time_step,input_size))
+	out[:,:,:]=X[0,:,:]
+	outresults.append(np.array(out[:,[0],:]))
+		
+	for i in range(len(X)-1):
+		outnew=model.predict(out)
+		if type(model.output_shape)==list:
+			outnew=outnew[0]+outnew[1]
+		out[:,0:time_step-1,:]=np.array(out[:,1:time_step,:])
+		out[:,-1,0]+=1*np.array(outnew[:,0,0])
+		out[:,-1,1]+=1*np.array(outnew[:,0,1])
+		out[:,-1,2]+=1*np.array(outnew[:,0,2])
+		out[:,:,3:]=X[i+1,:,3:]
+		if i%10000000==0 and i>0: #Occasional Datapoint to help tracking
+			out[:,:,:]=X[i+1,:,:]
+		outresults.append(np.array(out[:,[0],:]))
+
+	outresults=np.array(outresults)
+	outresults=np.reshape(outresults,(len(outresults),1,input_size))
+
+
+	for i in range(2):
+        	print(" Deep Model "+Name+" RMSE Error of State:",i,np.sqrt(np.mean((outresults[:,0,i]-X[:,0,i])**2)))
+
+	#Creating Animation of Tracking
+
+	fig, ax = plt.subplots()
+	xdata, ydata = [], []
+	xpdata, ypdata = [], []
+	ln, = plt.plot([], [], 'r', animated=True,label="True")
+	ln2, = plt.plot([], [], 'k', animated=True,linewidth="4",label="Predicted")
+	#Simple Functions to Help with Plotting
+	
+	def init():
+	        ax.set_xlim(0, 1600)	#Change 0 and 1600 based on expected limits of model
+	        ax.set_ylim(0, 1600)	#Change 0 and 1600 based on expected limits of model
+	        ax.set_ylabel("PY")
+	        ax.set_xlabel("PX")
+	        ax.legend()
+	        ln.set_data(xdata,ydata)
+	        ln2.set_data(xpdata,ypdata)
+	        return ln,ln2,
+	
+	def update(frame):
+	        xdata.append(frame[0])
+	        ydata.append(frame[1])
+	        xpdata.append(frame[2])
+	        ypdata.append(frame[3])
+	        ln.set_data(xdata, ydata)
+	        ln2.set_data(xpdata,ypdata)
+	        return ln,ln2,
+
+	ani = FuncAnimation(fig, update, frames=np.concatenate((X[:10000:10,0,0:2],outresults[:10000:10,0,0:2]),axis=1),init_func=init, blit=True, interval =1,repeat=False)
+	plt.show()
+	
+	mywriter = FFMpegFileWriter(fps=25,codec='mpeg4')
+	ani.save("Outputs/"+Name+".avi", writer=mywriter)
 
 
