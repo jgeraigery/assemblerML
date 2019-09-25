1d72cef3225e09a87f11e8416e04e02a74e23386
kentevan <kent.evans@parc.com>
2019-09-24 19:24:42 -0700

    fixing more errors

--- src/dPend/main.py
+++ src/dPend/main.py
@@ -30,10 +30,13 @@ class DPend():
         self.g = 9.81
 
     def getTh1dd(self):
-        return ( 1 / ( (self.m2*self.l1*np.power(np.cos(self.th1-self.th2),2)) - ((self.m1+self.m2)*self.l1) ) ) * ( (self.m2*self.l2*self.th2d*np.sin(self.th1-self.th2)) + (self.g*(self.m1+self.m2)*np.sin(self.th1)) + (self.m2*self.l2*np.cos(self.th1-self.th2))*( ( (self.m2*self.l1*np.power(self.th1d,2)*np.sin(self.th1-self.th2)) - (self.m2*self.g*np.sin(self.th2)) ) / (self.m2*self.l2) ))
+        #return ( 1 / ( (self.m2*self.l1*np.power(np.cos(self.th1-self.th2),2)) - ((self.m1+self.m2)*self.l1) ) ) * ( (self.m2*self.l2*self.th2d*np.sin(self.th1-self.th2)) + (self.g*(self.m1+self.m2)*np.sin(self.th1)) + (self.m2*self.l2*np.cos(self.th1-self.th2))*( ( (self.m2*self.l1*np.power(self.th1d,2)*np.sin(self.th1-self.th2)) - (self.m2*self.g*np.sin(self.th2)) ) / (self.m2*self.l2) ))
+        return ( ((self.m1+self.m2)*self.g*np.sin(self.th1)) - (self.m2*self.l2*np.power(self.th2d,2)*np.sin(self.th2-self.th1)) - ((self.m2*np.cos(self.th2-self.th1))*( (self.l1*np.power(self.th1d,2))*np.sin(self.th2-self.th1) + self.g*np.sin(self.th2) ) ) ) / ( (self.m2*self.l1*np.power(np.cos(self.th2-self.th1),2) - (self.m1+self.m2)*self.l1) )
+
 
     def getTh2dd(self):
-        return - ( (self.m2*self.l1*self.th1dd*np.cos(self.th1-self.th2)) - (self.m2*self.l1*np.power(self.th1d,2)*np.sin(self.th1-self.th2)) + (self.m2*self.g*np.sin(self.th2)) ) / (self.m2*self.l2)
+        #return - ( (self.m2*self.l1*self.th1dd*np.cos(self.th1-self.th2)) - (self.m2*self.l1*np.power(self.th1d,2)*np.sin(self.th1-self.th2)) + (self.m2*self.g*np.sin(self.th2)) ) / (self.m2*self.l2)
+        return ( (-self.l1*self.th1dd*np.cos(self.th2-self.th1)) - (self.l1*np.power(self.th1d,2)*np.sin(self.th2-self.th1)) - (self.g*np.sin(self.th2)) ) / (self.l2)
 
     def getM1X(self,th1):
         return self.l1*np.sin(th1)
@@ -55,6 +58,7 @@ class DPend():
             self.results[1,i] = self.th2
             self.results[2,i] = self.th1d
             self.results[3,i] = self.th2d
+
             self.results[4,i] = self.th1dd
             self.results[5,i] = self.th2dd
 
@@ -65,14 +69,12 @@ class DPend():
 #            self.th2d += self.th2dd*self.dt
             self.th1dd = self.getTh1dd()
             self.th2dd = self.getTh2dd()
+            self.th1d += self.th1dd *self.dt
+            self.th2d += self.th2dd *self.dt
             self.th1 += self.th1d * self.dt
             self.th2 += self.th2d * self.dt
-            self.th1d += self.th1dd*self.dt
-            self.th2d += self.th2dd*self.dt
 
             #print(self.results[:, i])
-
-
         return
 
         #print(np.cos(self.th1-self.th2))
@@ -87,12 +89,17 @@ class DPend():
         y2 = self.getM2Y(self.results[0,:],self.results[1,:])
 
         for i in range(int(self.results.shape[1]/step)):
-            x = [0 , x1[i*step], x2[i*step]]
-            y = [0 , y1[i*step], y2[i*step]]
+            if (i*step <= 1500):
+                lowLim = 0
+            else:
+                lowLim = i*step - 1500
+
+            x = [0, x1[i*step], x2[i*step]]
+            y = [0, y1[i*step], y2[i*step]]
 
             plt.plot(x, y, 'ro-')
-            ax.scatter(x1[0:i*step], y1[0:i*step])
-            ax.scatter(x2[0:i*step], y2[0:i*step])
+            ax.scatter(x1[lowLim:i*step], y1[lowLim:i*step], s = 1)
+            ax.scatter(x2[lowLim:i*step], y2[lowLim:i*step], s = 1)
             ax.set(xlim=(-15, 15), ylim=(-15, 15))
             plt.pause(.001)
             ax.clear()
@@ -107,11 +114,11 @@ class DPend():
         diffy = fig1.add_subplot(326)
 
         x1 = self.getM1X(self.results[0, :])
-        y1 = self.getM1Y(self.results[0,:])
+        y1 = self.getM1Y(self.results[0, :])
         x2 = self.getM2X(self.results[0, :], self.results[1, :])
         y2 = self.getM2Y(self.results[0, :], self.results[1, :])
 
-        T = np.linspace(0,self.t-self.dt,self.t/self.dt)
+        T = np.linspace(0, self.t-self.dt, self.t/self.dt)
         m1x.plot(T, x1)
         m1y.plot(T, y1)
         m2x.plot(T, x2)
@@ -120,9 +127,14 @@ class DPend():
         diffy.plot(T, y1-y2)
         plt.show()
 
-tester = DPend(1,2,3,1,np.pi/3,np.pi/2,0,0,.001,30,name="10-1-10-.5_" + dat.datetime.now().strftime("%Y%m%d-%H%M%S"))
+m1 = 2
+m2 = 3
+l1 = 2
+l2 = 3
+
+tester = DPend(m1,m2,l1,l2,np.pi/3,np.pi/2,0,0,.001,10,name= str(m1) + "-" + str(m2) + "-" + str(l1) + "-" + str(l2) + "_" + dat.datetime.now().strftime("%Y%m%d-%H%M%S"))
 tester.simulate()
 tester.plot()
 tester.animate(30)
 
-#np.savetxt(tester.name+".csv", tester.results, fmt='%.18e', delimiter=',', newline='\n', header='', footer='', comments='# ', encoding=None)
+np.savetxt(tester.name+".csv", tester.results, fmt='%.18e', delimiter=',', newline='\n', header='', footer='', comments='# ', encoding=None)
